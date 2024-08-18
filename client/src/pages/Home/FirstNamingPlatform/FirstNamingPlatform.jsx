90e91fe9ad9dee9ae6812e7c3acd42ce523b8950
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-08-18 19:03:24 +0300

    Refactor staic path

--- client/src/pages/Home/FirstNamingPlatform/FirstNamingPlatform.jsx
+++ client/src/pages/Home/FirstNamingPlatform/FirstNamingPlatform.jsx
@@ -1,5 +1,8 @@
 import { Link } from 'react-router-dom';
-import data from './data.json';
+import data from './data';
+import CONSTANTS from './../../../constants';
+
+const { STATIC_IMAGES_PATH } = CONSTANTS;
 
 console.log(data);
 
@@ -17,7 +20,7 @@ function FirstNamingPlatform () {
       <h2>Atom: The World's #1 Business Naming Platform</h2>
       <ul>{data.map(mapItems)}</ul>
       <div>
-        <img src='/staticImages/home/nh-theworld5.svg' alt='icon' />
+        <img src={`${STATIC_IMAGES_PATH}home/nh-theworld5.svg`} alt='icon' />
         <p>The Ultra Premium Marketplace Is Here</p>
         <Link to='#'>Explore now</Link>
       </div>
